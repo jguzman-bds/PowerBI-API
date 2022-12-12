f3a012ccae472bdb988b0c6d67d114b67ca99df5
Johan Guzman <115578368+jguzman-bds@users.noreply.github.com>
2022-12-12 14:49:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
+from azure.identity import ClientSecretCredential, InteractiveBrowserCredential, UsernamePasswordCredential
+import requests
+
+
+scope = [scope]
+
+
+import msal
+
+
+def get_token_username_password(scope):
+    app = msal.PublicClientApplication(client_id, authority=authority_url)
+    result = app.acquire_token_by_username_password(username=username,password=password,scopes=scope)
+    if 'access_token' in result:
+        return(result['access_token'])
+    else:
+        print('Error in get_token_username_password:',result.get("error"), result.get("error_description"))
+        
+token_string = get_token_username_password(scope)
+auth_token = token_string
+
+##
+
 import json
 import pandas as pd
 import requests
-import streamlit as st
-from azure.identity import ClientSecretCredential, InteractiveBrowserCredential, UsernamePasswordCredential
 
-### BORRAR
-scope = 'https://analysis.windows.net/powerbi/api/.default'
-interactive_browser_credential_class = InteractiveBrowserCredential()
-access_token_class = interactive_browser_credential_class.get_token(scope)
-token_string = access_token_class.token
-###
 
-#auth_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvOGQzMmE0MjgtOThlOS00YTE0LTgyNjItOTQ0ZDJmNTIwYjc4LyIsImlhdCI6MTY2OTIyNjk2NCwibmJmIjoxNjY5MjI2OTY0LCJleHAiOjE2NjkyMzE1NTQsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFURmN2dzd6NmkycXZOaTY0OFpzc2wzWnBUSEgyQ0dZWTdYUDhMMXhISUZETUxFN1BhZTdROWNycFQ5YkxaakRpWmMzM0w4N1NGTWFBRm14UnU4ZGw3MDRQK0tYVmV2bHJRdEZnSWRTc3RlND0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiMDRiMDc3OTUtOGRkYi00NjFhLWJiZWUtMDJmOWUxYmY3YjQ2IiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJHdXptYW4iLCJnaXZlbl9uYW1lIjoiSm9oYW4iLCJpcGFkZHIiOiI4MS40NS40MC4xMSIsIm5hbWUiOiJKb2hhbiBHdXptYW4iLCJvaWQiOiJjMjQyMzczYS02ZWFiLTRlZGMtODIxOC03MzRmNDI3MTg2MjciLCJwdWlkIjoiMTAwMzIwMDIzMjMxMDc0QSIsInJoIjoiMC5BVWdBS0tReWplbVlGRXFDWXBSTkwxSUxlQWtBQUFBQUFBQUF3QUFBQUFBQUFBQklBTncuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiVDRoblozSU9ROWZBVF8yel9ubldfc0JCSWJrc0NTbmp1cm5JS09LRjlKYyIsInRpZCI6IjhkMzJhNDI4LTk4ZTktNGExNC04MjYyLTk0NGQyZjUyMGI3OCIsInVuaXF1ZV9uYW1lIjoiai5ndXptYW5AYmRpZ2l0YWxzb2x1dGlvbnMuY29tIiwidXBuIjoiai5ndXptYW5AYmRpZ2l0YWxzb2x1dGlvbnMuY29tIiwidXRpIjoiRGRYY2hWUFg1VS1kSEFjalJFS1lBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il0sInhtc19jYyI6WyJDUDEiXX0.G8Hh7RY9m7Sg8S7i1V01mUBEwA7N1b3K5UP9DAoAdIjIIhDJqrpPp1ze8IUywVvzB6gV0RL0n3x3wx3cqtMeZNfx5PeehStDmS47ibATxhG8t0oRRowHGCPBW6qhvlfe2vY8gZDJF6ciroBQeMTNJtpgscLCLCBUBv4bay8E4W9V3yOl-kE6xCuoysU-ZzEWHUnTXNFX5GUpbvI80_F45R09XVyjCMp_i-Wz2tyooLyyTzEP6Ce_nHi-8hGF9OONcYGa17xAiicFDEXv4rVbqzknPQc5iBwpCtAbFdkhIGcLE4t43e53DnKF-5V5UCWV1_XqlXRX7I5yxZg4EKW04Q"
-auth_token = token_string
-dataset = "bc8741ce-3063-4647-9b1e-34c5a855fc8e"
+dataset = '1253097e-29d1-4082-87e2-66f45a2ceb95'
 
-# Post Request method to run the query against the XMLA dataset endpoint returning a pandas dataframe parsed from json response 
+
 def post_dax_query(query, auth_token, dataset):
     try: 
         url= "https://api.powerbi.com/v1.0/myorg/datasets/"+dataset+"/executeQueries"        
@@ -31,23 +44,82 @@ def post_dax_query(query, auth_token, dataset):
         #build a dataframe from the collected data
         df = pd.DataFrame(data=datos, columns=columnas)
         print(df.head())
-        return res
+        return response_to_pandas(res)
+        #return res
     except requests.exceptions.HTTPError as ex:
         print(ex)
     except Exception as e:
         print(e)
         
-# Executions to make it happen
-#auth_token = get_auth_token(power_bi_client_id, power_bi_username, power_bi_password)
+        
+def response_to_pandas(res):
+    data = res.json()
+    pandasdf = pd.json_normalize(data['results'][0]['tables'], record_path =['rows'])
+    return pandasdf
 
-# Write your DAX  query inside the query parameter. There you have an example.
-query = """EVALUATE
-TOPN(100, 'airline_passenger_satisfaction')"""
 
-# Capture dataframe in df 
-df = post_dax_query(query, auth_token, dataset)
+query_TIPORECO = """// DAX Query
+DEFINE
+  VAR __DS0FilterTable = 
+    FILTER(
+      KEEPFILTERS(VALUES('ConsultaReconocimientoElemento'[FECHARECO])),
+      AND(
+        'ConsultaReconocimientoElemento'[FECHARECO] >= DATE(2021, 1, 1),
+        'ConsultaReconocimientoElemento'[FECHARECO] < DATE(2022, 1, 1)
+      )
+    )
 
-data = df.json()
-type(data)
-pandasdf = pd.json_normalize(data['results'][0]['tables'], record_path =['rows'])
-st.write(pandasdf)
+  VAR __DS0Core = 
+    SUMMARIZECOLUMNS(
+      'ConsultaReconocimientoElemento'[TIPORECO],
+      __DS0FilterTable,
+      "DistinctCountID", CALCULATE(DISTINCTCOUNT('ConsultaReconocimientoElemento'[ID]))
+    )
+
+  VAR __DS0BodyLimited = 
+    TOPN(1002, __DS0Core, [DistinctCountID], 0, 'ConsultaReconocimientoElemento'[TIPORECO], 1)
+
+EVALUATE
+  __DS0BodyLimited
+
+ORDER BY
+  [DistinctCountID] DESC, 'ConsultaReconocimientoElemento'[TIPORECO]
+"""
+
+df_TIPORECO = post_dax_query(query_TIPORECO, auth_token, dataset)
+
+##
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+values_TIPORECO = df_TIPORECO['[DistinctCountID]']
+labels_TIPORECO = df_TIPORECO['ConsultaReconocimientoElemento[TIPORECO]']
+
+p_tipo_reco, a_tipo_reco = plt.subplots()
+
+l = a_tipo_reco.pie(values_TIPORECO, startangle=15)
+
+for label, t in zip(labels_TIPORECO, l[1]):
+    x, y = t.get_position()
+    angle = int(math.degrees(math.atan2(y, x)))
+    ha = "left"
+    va = "bottom"
+
+    if angle > 90:
+        angle -= 180
+
+    if angle < 0:
+        va = "top"
+
+    if -45 <= angle <= 0:
+        ha = "right"
+        va = "bottom"
+
+    plt.annotate(label, xy=(x,y), rotation=angle, ha=ha, va=va, size=8)
+
+p_tipo_reco.set_edgecolor('white')
+
+a_tipo_reco.set_title("Tipos de reconocimiento");
